ef1f0e3b185577bbeaa71b37eeaf79960e0c1898
GAEL_SEBASTIAN_ROJAS_MATA <a.22308051281303@cbtis128.edu.mx>
2025-03-04 16:55:32 +0000

    enviar

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.grey[200], // Fondo gris claro
         appBar: AppBar(
           title: Center(
-            // Centrar el título
             child: Text(
               'Gael Sebastian Rojas Mata 22308051281303', // Título de la AppBar
               style: TextStyle(
-                color: Color(0xffffffff), // Color azul marino
-                fontSize: 15, // Tamaño más grande
-                fontWeight: FontWeight.bold,
+                color: Colors.white, // Color del texto blanco
+                fontSize: 15,
               ),
             ),
           ),
@@ -26,14 +25,15 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           child: Container(
-            width: 170,
+            width: 200,
             height: 100,
             decoration: BoxDecoration(
-              color: Color(0xffbce1ff), // Color azul del contenedor
+              color: Color(
+                  0xff9cc3e0), // Color azul del contenedor (igual al AppBar)
               borderRadius: BorderRadius.circular(20), // Esquinas redondeadas
               boxShadow: [
                 BoxShadow(
-                  color: Colors.blue.shade700.withOpacity(1), // Sombra verde
+                  color: Colors.blue.shade900.withOpacity(1), // Sombra azul
                   spreadRadius: 2,
                   blurRadius: 5,
                   offset: Offset(4, 4), // Sombra hacia abajo y a la derecha
@@ -44,9 +44,8 @@ class MyApp extends StatelessWidget {
               child: Text(
                 'I am a container', // Texto dentro del contenedor
                 style: TextStyle(
-                  color: Color(0xff135979), // Color del texto
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
+                  color: Color(0xff22687e), // Color del texto blanco
+                  fontSize: 28,
                 ),
               ),
             ),
